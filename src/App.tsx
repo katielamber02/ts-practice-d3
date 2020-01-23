80f18b91f5fc5c39bfbbd86563561ed2cf3159d2
Katie Lamber <katielamber02@gmail.com>
2020-01-23 14:51:10 +0200

    D3 initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React, { useRef } from "react";
-import logo from "./logo.svg";
+import React, { useRef, useEffect } from "react";
+
 import "./App.css";
+
 import { select } from "d3-selection";
-import {select} from 'd3-selection'
 
 const App: React.FC = () => {
-  const svgRef=useRef<SVGSVGElement>(null)
-  useEffect(()=>{
-    console.log(select(svgRef.current)
-  })
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    console.log(select(svgRef.current));
+  });
+
+  //   Selection {_groups: Array(1), _parents: Array(1)}
+  // _groups: Array(1)
+  // 0: [svg]
+  // length: 1
+
   return (
     <div className="App">
-    <svg ref={svgRef}/>
+      <svg ref={svgRef} />
     </div>
   );
 };
